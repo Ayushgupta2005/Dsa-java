427b443f52f6c3f477962a43c71c2b9e48e9fa9d
ayushgupta2005 <ayushguptaprasad@gmail.com>
2026-02-24 19:54:12 +0530

    factorial of a number

---
+++ math/Factorial.java
@@ -0,0 +1,16 @@
+package math;
+
+public class Factorial {
+    public static long factorial(int n) {
+        long fact = 1;
+        for (int i = 2; i <= n; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+    }
+    
+}
