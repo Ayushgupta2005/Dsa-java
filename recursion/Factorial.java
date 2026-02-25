bac0fbe9003b0700fee009a18dbe55262b481178
ayushgupta2005 <ayushguptaprasad@gmail.com>
2026-02-25 23:28:00 +0530

    Factorial of a number using recursion

---
+++ recursion/Factorial.java
@@ -0,0 +1,12 @@
+package recursion;
+
+class Factorial {
+    public int factorial(int n) {
+        // Base case
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        // Recursive call
+        return n * factorial(n - 1);
+    }
+}
