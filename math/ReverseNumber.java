9857902367afcc2aef6e2931af77972471a24879
ayushgupta2005 <ayushguptaprasad@gmail.com>
2026-03-04 23:37:25 +0530

    Reverse a number

---
+++ math/ReverseNumber.java
@@ -0,0 +1,18 @@
+public class ReverseNumber {
+
+    public static void main(String[] args) {
+        
+        int n =1625;
+        int rev=0;
+
+        while (n>0){
+            rev = rev * 10;
+            rev = rev + n% 10;
+            n = n/10;
+
+        }
+        System.out.println(" Reversed numeber " + rev
+        );
+    }
+    
+}
