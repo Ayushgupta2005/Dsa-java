d1cee2b277581818dd0dfe964ad0d76952435458
ayushgupta2005 <ayushguptaprasad@gmail.com>
2026-03-29 21:58:09 +0530

    square alphabet 2

---
+++ patterns/Square_Albhabet2.java
@@ -0,0 +1,13 @@
+public class Square_Albhabet2 {
+    public static void main(String[] args) {
+        for (int i=0; i<4; i++){
+            System.out.println();
+            for(int j=0; j<4; j++){
+                System.out.print((char)(j+97) + " ");
+            }
+        }
+        
+
+    }
+    
+}
