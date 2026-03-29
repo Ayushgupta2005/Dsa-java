000a90c36d30ea152d4b81dca8ba85f3db766480
ayushgupta2005 <ayushguptaprasad@gmail.com>
2026-03-29 22:03:18 +0530

    square number pattern

---
+++ patterns/Square_number.java
@@ -0,0 +1,18 @@
+/*
+Write a program to print the following pattern:
+1111
+2222
+3333
+4444 */
+public class Square_number {
+    public static void main(String[] args) {
+        
+        for (int i=0; i<4; i++){
+            System.out.println();
+            for(int j=0; j<4; j++){
+                System.out.print(i+1);
+            }
+        }
+    }
+    
+}
