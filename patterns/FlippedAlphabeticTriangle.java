afd4418784982fffad1fdaa216a5ead254173cbf
ayushgupta2005 <ayushguptaprasad@gmail.com>
2026-04-02 16:38:16 +0530

    Flipped Alphabetic Triangle pattern

---
+++ patterns/FlippedAlphabeticTriangle.java
@@ -0,0 +1,17 @@
+public class FlippedAlphabeticTriangle {
+    public static void main(String[] args) {
+        
+        for(int i=4; i>0; i--){
+            System.out.println();
+            for(int j=0; j<4; j++){
+                if(j<i-1){
+                    System.out.print("  ");
+                }
+                else{
+                    System.out.print((char)(69-i));
+                }
+            }
+        }
+    }
+    
+}
