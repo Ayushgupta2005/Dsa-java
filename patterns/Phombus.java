1e71874d218a41aa63e99828d1e13a9133bb694f
ayushgupta2005 <ayushguptaprasad@gmail.com>
2026-04-02 17:17:00 +0530

    Phombus Pattern

---
+++ patterns/Phombus.java
@@ -0,0 +1,15 @@
+public class Phombus {
+    public static void main(String[] args) {
+
+        
+        for(int i=4; i>0; i--){
+            System.out.println();
+
+            for(int j=i-1;j>0; j--){
+                System.out.print(" ");
+            }
+            System.out.print("****");
+            }
+        }
+    }
+    
