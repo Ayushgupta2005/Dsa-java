b994cb5321cad32954ab6effdbfa4faa7489eac8
ayushgupta2005 <ayushguptaprasad@gmail.com>
2026-04-07 16:34:47 +0530

    Sorting practice

---
+++ arrays/Practice.java
@@ -0,0 +1,28 @@
+public class Practice {
+    public static void main(String[] args) {
+        
+        int arr[] ={5,3,2,1,8};
+
+        for(int i=0; i<arr.length; i++){
+            int minIndex=i;
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[j]<arr[minIndex]){
+                    minIndex=j;
+                }
+
+            }
+
+            // Swap operation 
+            int temp =arr[i];
+            arr[i]=arr[minIndex];
+            arr[minIndex]=temp;
+        }
+
+
+        for(int i=0; i<arr.length ;i++){
+            System.out.print(arr[i]+ " ");
+
+        }
+    }
+    
+}
