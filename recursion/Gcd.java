6a5019a619ea9e10bf37c47e34716f660215366c
ayushgupta2005 <ayushguptaprasad@gmail.com>
2026-04-22 22:03:52 +0530

    GCD using recursion

---
+++ recursion/Gcd.java
@@ -0,0 +1,15 @@
+public class Gcd {
+
+    public static int hcf(int a, int b){
+        if(a==0){
+            return b;
+        }
+        return hcf(b%a, a);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(hcf(24,60));
+    }
+    
+}
