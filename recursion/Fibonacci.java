b928164a5ca36f2c083451b029a74e9adb221edf
ayushgupta2005 <ayushguptaprasad@gmail.com>
2026-04-23 23:57:09 +0530

    recursion/Fibonacci.java

---
+++ recursion/Fibonacci.java
@@ -0,0 +1,22 @@
+// Nth fibonacci number 
+public class Fibonacci {
+
+    public static int fibo(int n){
+        if(n==0){
+            return 0;
+        }
+        else if(n==1){
+            return 1;
+        }
+
+        return fibo(n-1)+fibo(n-2);
+    }
+
+    public static void main(String[] args) {
+
+        int n=7;
+        System.out.println(fibo(n));
+        
+    }
+    
+}
