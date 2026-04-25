08758a772763d82a5675c6abb21d48d4a66c1352
ayushgupta2005 <ayushguptaprasad@gmail.com>
2026-04-26 00:51:24 +0530

    DSA

---
+++ arrays/Binary_Search/FloorInSortedArray.java
@@ -0,0 +1,38 @@
+public class FloorInSortedArray {
+    // find the index (0-based) of the largest element in arr[]
+    // that is less than or equal to x.
+    // Time complexity (log n) ----> Binary Search Algorithm 
+
+
+    public static int findFloor(int[] arr, int x){
+        int i=0; 
+        int j=arr.length-1;
+        int idx =-1;
+        
+        while(i<=j){
+            int mid = i+(j-i)/2;
+            if(arr[mid]>x){
+                j=mid-1;
+            }
+            else if(arr[mid]<=x){
+                idx =mid;
+                i=mid+1;
+            }
+        }
+    return idx;
+        
+    }
+
+    public static void main(String[] args) {
+        
+        int arr[] ={1,2,8,10,10,19};
+
+        int ans = findFloor(arr, 5);
+
+        System.out.println(ans);
+
+
+    }
+        
+    }
+    
--- recursion/Recursion.java
+++ recursion/Recursion.java
@@ -1,19 +1,37 @@
-package recursion;
-
 public class Recursion {
 
-    static void Print(int n){
-        if(n==10){
-            return;
+    public static void main(String[] args) {
+        
+        int arr[] = {2,3,7,8,10};
+
+        int i=0;
+        int j=arr.length-1;
+        int target =9;
+        boolean found =false;
+
+        while(i<=j){
+            int mid = i+(j-i)/2;
+
+            if(arr[mid]==target){
+                System.out.println("element found at index " + mid);
+                found = true;
+                break;
+        }
+        else if(arr[mid]>target){
+            j=mid-1;
+        }
+        else{
+            i=mid+1;
         }
 
-        System.out.println(" Ayush");
-        Print(n+1);
     }
 
-    public static void main(String[] args) {
-        Print(1);
+    if(found==false){
+        System.out.println("Element not found");
+    }
+
+
+    
+    }
+    }
         
-
-    }
-}
