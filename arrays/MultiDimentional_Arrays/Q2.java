8f98ef8007af6839dbddc21402080869d144cde4
ayushgupta2005 <ayushguptaprasad@gmail.com>
2026-04-30 23:08:10 +0530

    2D Array question

---
+++ arrays/MultiDimentional_Arrays/Q2.java
@@ -0,0 +1,28 @@
+package MultiDimentional_Arrays;
+
+
+// Find the minimum element out of all the maximum elements of each row
+public class Q2 {
+    public static void main(String[] args) {
+        int arr[][] = {{2,8 ,3,4,7}, {7,2,1,6,3},{5,5,4,1,4},{3,1,8,2,6}};
+
+        int min = Integer.MAX_VALUE;
+
+        for(int i=0; i<arr.length; i++){
+            int max=Integer.MIN_VALUE;
+            for(int j=0; j<arr[i].length; j++){
+                if(arr[i][j]>max){
+                    max= arr[i][j];
+                }
+            }
+
+            if(max<min){
+                min = max;
+            }
+        }
+        System.out.println(min);
+
+
+    }
+    
+}
